93ef77e446edeb57600e716bd9303b9c05f70263
= <=>
2024-08-13 11:43:08 +0530

    quiz features: export the missing states to csv using pandas

--- main.py
+++ main.py
@@ -19,14 +19,26 @@ data = pandas.read_csv("50_states.csv")
 states = data.state.to_list()
 
 while len(guessed_state) < 50:
+
     # getting user input
-    answer = screen.textinput(f"Guess the State {len(guessed_state)}/50", "What's another state's name? ")
-    answer = answer.title()
+    answer = screen.textinput(f"Guess the State {len(guessed_state)}/50", "What's another state's name? ").title()
+
+    # user wants to exit
     if answer == "Exit":
+        s1 = set(states)
+        s2 = set(guessed_state)
+        diff = list(s1 - s2)
+        states_to_learn = pandas.DataFrame(diff)
+        states_to_learn.to_csv("states_to_learn.csv")
         break
+
+    # checking if guessed answer is correct and exist in data
     if answer in states:
+
+        # adding to guessed answers list
         guessed_state.append(answer)
+
+        # getting the coordinates and writing the state names
         row = data[data.state == answer]
         turtle.goto(int(row.x), int(row.y))
         turtle.write(arg=f"{answer}", align=ALIGNMENT, font=FONT)
-
